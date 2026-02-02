1a3f861059dd3f500e4cd81c9cfbd58a57cd47d4
waninko <linn.eidsten@gmail.com>
2026-02-02 11:35:06 +0100

    show list of project tasks
    project component, JSON server, fun material icons

---
+++ src/components/SingleProject.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="project">
+        <div class="actions">
+            <h3 @click="showDetails = !showDetails">{{ project.title }}</h3>
+            <div class="icons">
+                <span class="material-icons">edit</span>
+                <span class="material-icons">delete</span>
+                <span class="material-icons">done</span>
+            </div>
+        </div>
+        <div class="details" v-if="showDetails">
+            <p>{{ project.details }}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['project'],
+    data() {
+        return {
+            showDetails: false,
+        }
+    },
+}
+</script>
+
+<style>
+.project {
+    margin: 20px auto;
+    background: whitesmoke;
+    padding: 10px 20px;
+    border-radius: 5px;
+    box-shadow: 1px 2px 3px rgba(0, 0, 0, 0.729);
+    border-left: 4px solid rgb(255, 89, 89);
+}
+
+h3 {
+    cursor: pointer;
+}
+
+.actions {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+
+.material-icons {
+    font-size: 25px;
+    margin-left: 10px;
+    color: #bbb;
+    cursor: pointer;
+}
+
+.material-icons:hover {
+    color: #777;
+}
+</style>
